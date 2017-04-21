525f459fd18e08a21bb7c55aeb4a5ec28b7fb46d
xaanit <shadowjacob@yahoo.com>
2017-04-20 23:51:56 -0500

    Formatting

--- src/main/java/com/github/xaanit/d4j/oauth/D4JOAuth.java
+++ src/main/java/com/github/xaanit/d4j/oauth/D4JOAuth.java
@@ -5,27 +5,27 @@ import sx.blah.discord.modules.IModule;
 
 public class D4JOAuth implements IModule {
 
-    public boolean enable(IDiscordClient client) {
-        return true;
-    }
+	public boolean enable(IDiscordClient client) {
+		return true;
+	}
 
-    public void disable() {
+	public void disable() {
 
-    }
+	}
 
-    public String getName() {
-        return "D4J-OAuth";
-    }
+	public String getName() {
+		return "D4J-OAuth";
+	}
 
-    public String getAuthor() {
-        return "xaanit";
-    }
+	public String getAuthor() {
+		return "xaanit";
+	}
 
-    public String getVersion() {
-        return "0.1";
-    }
+	public String getVersion() {
+		return "0.1";
+	}
 
-    public String getMinimumDiscord4JVersion() {
-        return "2.7.0";
-    }
+	public String getMinimumDiscord4JVersion() {
+		return "2.7.0";
+	}
 }
--- src/main/java/com/github/xaanit/d4j/oauth/Scope.java
+++ src/main/java/com/github/xaanit/d4j/oauth/Scope.java
@@ -1,16 +1,27 @@
 package com.github.xaanit.d4j.oauth;
 
 public enum Scope {
-    BOT("bot"), CONNECTIONS("connections"), EMAIL("email"), INDETIFY("identify"), GUILDS("guilds"), GUILDS_JOIN("guilds.join"), GDM_JOIN("gdm.join"), MESSAGES_READ("message.read"), RPC("rpc"), RPC_API("rpc.api"), RPC_NOTIFICATIONS_READ("rpc.notifications.read"), WEBHOOK_INCOMING("webhook.incoming");
+	BOT("bot"),
+	CONNECTIONS("connections"),
+	EMAIL("email"),
+	INDETIFY("identify"),
+	GUILDS("guilds"),
+	GUILDS_JOIN("guilds.join"),
+	GDM_JOIN("gdm.join"),
+	MESSAGES_READ("message.read"),
+	RPC("rpc"),
+	RPC_API("rpc.api"),
+	RPC_NOTIFICATIONS_READ("rpc.notifications.read"),
+	WEBHOOK_INCOMING("webhook.incoming");
 
-    private String name;
+	private String name;
 
-    Scope(String name) {
-        this.name = name;
-    }
+	Scope(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	public String getName() {
+		return this.name;
+	}
 
 }
