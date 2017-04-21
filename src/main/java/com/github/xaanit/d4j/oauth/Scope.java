4cbca670fe83a3dd8471b2f0e6f8fe7f60c8e06f
xaanit <shadowjacob@yahoo.com>
2017-04-20 19:24:40 -0500

    Made Scope enum, should've gotten everything

---
+++ src/main/java/com/github/xaanit/d4j/oauth/Scope.java
@@ -0,0 +1,16 @@
+package com.github.xaanit.d4j.oauth;
+
+public enum Scope {
+    BOT("bot"), CONNECTIONS("connections"), EMAIL("email"), INDETIFY("identify"), GUILDS("guilds"), GUILDS_JOIN("guilds.join"), GDM_JOIN("gdm.join"), MESSAGES_READ("message.read"), RPC("rpc"), RPC_API("rpc.api"), RPC_NOTIFICATIONS_READ("rpc.notifications.read"), WEBHOOK_INCOMING("webhook.incoming");
+
+    private String name;
+
+    Scope(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
