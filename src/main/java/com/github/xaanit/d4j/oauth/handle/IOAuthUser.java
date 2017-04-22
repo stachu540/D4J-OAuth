b1f7d22fa3e9684f3d4bfefccc9f94dddddcbef5
undermybrella <undermybrella@abimon.org>
2017-04-22 12:40:59 +1000

    Implemented OAuthUser

--- src/main/java/com/github/xaanit/d4j/oauth/handle/IOAuthUser.java
+++ src/main/java/com/github/xaanit/d4j/oauth/handle/IOAuthUser.java
@@ -6,5 +6,10 @@ import sx.blah.discord.handle.obj.IUser;
  * Created by Jacob on 4/21/2017.
  */
 public interface IOAuthUser extends IUser {
-
+	/**
+	 * Get the access token for this user
+	 *
+	 * @return the access token
+	 */
+	String getAccessToken();
 }
--- src/main/java/com/github/xaanit/d4j/oauth/handle/impl/OAuthUser.java
+++ src/main/java/com/github/xaanit/d4j/oauth/handle/impl/OAuthUser.java
@@ -1,8 +1,161 @@
 package com.github.xaanit.d4j.oauth.handle.impl;
 
+import com.github.xaanit.d4j.oauth.handle.IOAuthUser;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.IShard;
+import sx.blah.discord.handle.obj.*;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by Jacob on 4/21/2017.
  */
-public class OAuthUser {
+public class OAuthUser implements IOAuthUser {
+	private final IUser user;
+	private final String accessToken;
 
+	public OAuthUser(IUser user, String accessToken) {
+		this.user = user;
+		this.accessToken = accessToken;
+	}
+
+	@Override
+	public String getName() {
+		return user.getName();
+	}
+
+	@Override
+	public Status getStatus() {
+		return user.getStatus();
+	}
+
+	@Override
+	public String getAvatar() {
+		return user.getAvatar();
+	}
+
+	@Override
+	public String getAvatarURL() {
+		return user.getAvatarURL();
+	}
+
+	@Override
+	public Presences getPresence() {
+		return user.getPresence();
+	}
+
+	@Override
+	public String getDisplayName(IGuild iGuild) {
+		return user.getDisplayName(iGuild);
+	}
+
+	@Override
+	public String mention() {
+		return user.mention();
+	}
+
+	@Override
+	public String mention(boolean b) {
+		return user.mention(b);
+	}
+
+	@Override
+	public String getDiscriminator() {
+		return user.getDiscriminator();
+	}
+
+	@Override
+	public List<IRole> getRolesForGuild(IGuild iGuild) {
+		return user.getRolesForGuild(iGuild);
+	}
+
+	@Override
+	public EnumSet<Permissions> getPermissionsForGuild(IGuild iGuild) {
+		return user.getPermissionsForGuild(iGuild);
+	}
+
+	@Override
+	public Optional<String> getNicknameForGuild(IGuild iGuild) {
+		return user.getNicknameForGuild(iGuild);
+	}
+
+	@Override
+	public boolean isBot() {
+		return user.isBot();
+	}
+
+	@Override
+	public void moveToVoiceChannel(IVoiceChannel iVoiceChannel) throws DiscordException, RateLimitException, MissingPermissionsException {
+		user.moveToVoiceChannel(iVoiceChannel);
+	}
+
+	@Override
+	public List<IVoiceChannel> getConnectedVoiceChannels() {
+		return user.getConnectedVoiceChannels();
+	}
+
+	@Override
+	public IPrivateChannel getOrCreatePMChannel() throws RateLimitException, DiscordException {
+		return user.getOrCreatePMChannel();
+	}
+
+	@Override
+	public boolean isDeaf(IGuild iGuild) {
+		return user.isDeaf(iGuild);
+	}
+
+	@Override
+	public boolean isMuted(IGuild iGuild) {
+		return user.isMuted(iGuild);
+	}
+
+	@Override
+	public boolean isDeafLocally() {
+		return user.isDeafLocally();
+	}
+
+	@Override
+	public boolean isMutedLocally() {
+		return user.isMutedLocally();
+	}
+
+	@Override
+	public void addRole(IRole iRole) throws MissingPermissionsException, RateLimitException, DiscordException {
+		user.addRole(iRole);
+	}
+
+	@Override
+	public void removeRole(IRole iRole) throws MissingPermissionsException, RateLimitException, DiscordException {
+		user.removeRole(iRole);
+	}
+
+	@Override
+	public String getID() {
+		return user.getID();
+	}
+
+	@Override
+	public IDiscordClient getClient() {
+		return user.getClient();
+	}
+
+	@Override
+	public IShard getShard() {
+		return user.getShard();
+	}
+
+	@Override
+	public IUser copy() {
+		return new OAuthUser(user.copy(), accessToken);
+	}
+
+	@Override
+	public String getAccessToken() {
+		return accessToken;
+	}
 }
