a4e6479185ba77a74a86b202d814f87173b5ed85
xaanit <shadowjacob@yahoo.com>
2017-04-30 18:30:27 -0500

    Added more webhook support

---
+++ src/main/java/com/github/xaanit/d4j/oauth/handle/IOauthWebhook.java
@@ -0,0 +1,65 @@
+package com.github.xaanit.d4j.oauth.handle;
+
+import sx.blah.discord.handle.obj.IUser;
+
+/**
+ * Created by Jacob on 4/30/2017.
+ */
+public interface IOauthWebhook {
+
+	/**
+	 * Gets the URL of the webhook
+	 *
+	 * @return The URL
+	 */
+	String getURL();
+
+	/**
+	 * Gets the ID of the webhook
+	 *
+	 * @return The ID
+	 */
+	String getID();
+
+	/**
+	 * Gets the long ID of the webhook's guild
+	 *
+	 * @return The ID as a long
+	 */
+	long getGuildLongID();
+
+	/**
+	 * Gets the String ID of the webhook's guild
+	 *
+	 * @return The ID as a string
+	 */
+	String getGuildStringID();
+
+	/**
+	 * Gets the {@link sx.blah.discord.handle.obj.IUser} object that posts the
+	 *
+	 * @return The user
+	 */
+	IUser getUser();
+
+	/**
+	 * Gets the name of the webhook
+	 *
+	 * @return The name
+	 */
+	String getName();
+
+	/**
+	 * Gets the Avatar URL of the webhook
+	 *
+	 * @return The avatar's URL
+	 */
+	String getAvatarURL();
+
+	/**
+	 * Gets the token of the webhook
+	 *
+	 * @return The token
+	 */
+	String getToken();
+}
---
+++ src/main/java/com/github/xaanit/d4j/oauth/handle/impl/OAuthWebhook.java
@@ -0,0 +1,69 @@
+package com.github.xaanit.d4j.oauth.handle.impl;
+
+import com.github.xaanit.d4j.oauth.handle.IOauthWebhook;
+import sx.blah.discord.handle.obj.IUser;
+
+/**
+ * Created by Jacob on 4/30/2017.
+ */
+public class OAuthWebhook implements IOauthWebhook {
+
+	private String url;
+	private String id;
+	private String guildID;
+	private IUser user;
+	private String name;
+	private String avatarURL;
+	private String token;
+
+	public OAuthWebhook(String url, String id, String guildID, IUser user, String name, String avatarURL, String token) {
+		this.url = url;
+		this.id = id;
+		this.guildID = guildID;
+		this.user = user;
+		this.name = name;
+		this.avatarURL = avatarURL;
+		this.token = token;
+	}
+
+
+	@Override
+	public String getURL() {
+		return this.url;
+	}
+
+	@Override
+	public String getID() {
+		return this.id;
+	}
+
+	@Override
+	public long getGuildLongID() {
+		return Long.parseUnsignedLong(getGuildStringID());
+	}
+
+	@Override
+	public String getGuildStringID() {
+		return this.id;
+	}
+
+	@Override
+	public IUser getUser() {
+		return this.user;
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String getAvatarURL() {
+		return this.avatarURL;
+	}
+
+	@Override
+	public String getToken() {
+		return this.token;
+	}
+}
